2093562c3b15df0ee17f3a221e3e3a2c4e1f2b61
haejunejung <jhj9422@unist.ac.kr>
2024-03-09 21:41:25 +0900

    viewport control

--- web-view/src/shared/Layout.tsx
+++ web-view/src/shared/Layout.tsx
@@ -22,17 +22,34 @@ export const Layout = ({ children }: Props) => {
     fetchColorPalette();
   }, []);
 
+  useEffect(() => {
+    const adjustHeight = () => {
+      // 실제 viewport 높이를 CSS 변수로 설정
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+
+    // 초기 로드 시와 창 크기 변경 시에 높이 조정
+    window.addEventListener("resize", adjustHeight);
+    adjustHeight(); // 초기 설정
+
+    // cleanup 함수에서 이벤트 리스너 제거
+    return () => window.removeEventListener("resize", adjustHeight);
+  }, []);
+
   return <Container>{children}</Container>;
 };
 
-const Container = styled.div(({ minHeight }: { minHeight?: number }) => ({
+const Container = styled.div(({}: { minHeight?: number }) => ({
   maxWidth: 390,
   marginLeft: "auto",
   marginRight: "auto",
-  minHeight: minHeight ?? "100vh",
+  minHeight: "calc(var(--vh, 1vh) * 100)",
   overflowY: "hidden",
   backgroundColor: "rgb(253, 253, 150)",
-  position: "relative", // Add this line to make the container position relative
+  position: "relative",
 }));
 
 export default Layout;
