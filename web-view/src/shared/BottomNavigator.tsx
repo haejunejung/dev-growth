820e0f8bb9fd46d71ac099efd09d25fa7c10bc3d
haejunejung <jhj9422@unist.ac.kr>
2024-03-16 12:10:46 +0900

    bottom navigator RSC

--- web-view/src/shared/BottomNavigator.tsx
+++ web-view/src/shared/BottomNavigator.tsx
@@ -1,61 +1,74 @@
-"use client";
-
-import { ROUTES, UIConstants } from "@/utils/constants";
+import { ROUTES } from "@/utils/constants";
 import BoxElement from "./BoxElement";
 import Link from "next/link";
 import { TextElement } from "./TextElement";
 import { HStack, VStack } from "./Stack";
 import useIconURLStore from "@/utils/stores/useIconURLStore";
-import useColorStore from "@/utils/stores/useColorPaletteStore";
-import styled from "@emotion/styled";
 
 const BottomNavigator = () => {
-  const colorPalette = useColorStore.getState().colorPalette;
   const iconURLs = useIconURLStore.getState().iconURLs;
 
   return (
     <HStack paddingHorizontal={20}>
-      <BottomContainer width={350} height={60}>
+      <BoxElement
+        width={350}
+        height={60}
+        style={{
+          alignItems: "center",
+          justifyContent: "space-around",
+          width: "100%",
+          maxWidth: 350,
+          padding: "24px 0",
+          position: "fixed",
+          bottom: 10,
+          zIndex: 2,
+          backgroundColor: "white",
+        }}
+      >
         <HStack
           gap={20}
           alignItems="center"
           justifyContent="space-around"
           height={"100%"}
         >
-          <Link href={ROUTES.YOUTUBE}>
-            <VStack alignItems="center">
-              <img src={iconURLs.IcYoutube ?? ""} width={24} height={24} />
-              <TextElement textStyle="C1">Youtube</TextElement>
-            </VStack>
-          </Link>
-          <Link href={ROUTES.GITHUB}>
-            <VStack alignItems="center">
-              <img src={iconURLs.IcGithub ?? ""} width={24} height={24} />
-              <TextElement textStyle="C1">Github</TextElement>
-            </VStack>
-          </Link>
-          <Link href={ROUTES.BLOG}>
-            <VStack alignItems="center">
-              <img src={iconURLs.IcBlog ?? ""} width={24} height={24} />
-              <TextElement textStyle="C1">Tech</TextElement>
-            </VStack>
-          </Link>
+          <TabItem
+            href={ROUTES.YOUTUBE}
+            src={iconURLs.IcYoutube ?? ""}
+            alt={"Youtube"}
+          />
+          <TabItem
+            href={ROUTES.GITHUB}
+            src={iconURLs.IcGithub ?? ""}
+            alt={"Github"}
+          />
+          <TabItem
+            href={ROUTES.BLOG}
+            src={iconURLs.IcBlog ?? ""}
+            alt={"Blog"}
+          />
         </HStack>
-      </BottomContainer>
+      </BoxElement>
     </HStack>
   );
 };
 
-export default BottomNavigator;
+const TabItem = ({
+  href,
+  src,
+  alt,
+}: {
+  href: string;
+  src: string;
+  alt: string;
+}) => {
+  return (
+    <Link href={href}>
+      <VStack alignItems="center">
+        <img src={src} width={24} height={24} />
+        <TextElement textStyle="C1">{alt}</TextElement>
+      </VStack>
+    </Link>
+  );
+};
 
-export const BottomContainer = styled(BoxElement)({
-  alignItems: "center",
-  justifyContent: "space-around",
-  width: "100%",
-  maxWidth: 350,
-  padding: "24px 0",
-  position: "fixed",
-  bottom: 10,
-  zIndex: 2,
-  backgroundColor: "white",
-});
+export default BottomNavigator;
