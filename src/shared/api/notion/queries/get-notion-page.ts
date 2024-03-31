1a11dd78931f348652613f4a27fcc7bb1e107787
haejunejung <jhj9422@unist.ac.kr>
2024-04-01 00:03:53 +0900

    fetchCollections

--- src/shared/api/notion/queries/get-notion-page.ts
+++ src/shared/api/notion/queries/get-notion-page.ts
@@ -2,6 +2,6 @@ import {NotionAPI} from 'notion-client';
 
 export default async function getNotionPage(pageId: string) {
   const notion = new NotionAPI();
-  const recordMap = await notion.getPage(pageId);
+  const recordMap = await notion.getPage(pageId, {fetchCollections: true});
   return recordMap;
 }
